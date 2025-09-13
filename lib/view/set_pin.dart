eb81c98d2df292d0daae66b051ab30bdb8af8e88
Rajeshwer Sahani <rajeshwersahani720@gmail.com>
2025-09-13 23:16:50 +0530

    create login passcode

--- lib/view/set_pin.dart
+++ lib/view/set_pin.dart
@@ -1,6 +1,7 @@
 import 'package:alieus_captials_09_08/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screen_lock/flutter_screen_lock.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 // import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 
@@ -10,9 +11,19 @@ void main(){
   runApp(MyApp());
 }
 
-class set_pin extends StatelessWidget{
+class set_pin extends StatefulWidget{
   const set_pin({super.key});
-  
+
+  @override
+  State<set_pin> createState() => _set_pinState();
+}
+
+class _set_pinState extends State<set_pin> {
+
+   String correctPin = "1234"; // You can store it securely
+  String enteredPin = "";
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +44,30 @@ class set_pin extends StatelessWidget{
                     Image.asset("assets/set_pin/ellipse_copy.png"), 
 
                     Positioned(top: 234,left: 105,
-                      child: Text("Enter your login passcode",style: TextStyle(color: Colors.white,fontSize: 17),) )
+                      child: Text("Enter your login passcode",style: TextStyle(color: Colors.white,fontSize: 17),
+                      ) 
+                      ),
 
+
+                    Positioned(
+                      child: PinCodeTextField(appContext: context, length: 4,
+                      obscureText: true,
+                       onChanged: (value) {
+                       enteredPin = value;
+                        },
+                         onCompleted: (value) {
+              if (value == correctPin) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Login Successful ✅")),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Invalid PIN ❌")),
+                );
+              }
+            },
+                      )
+                      )
                       
                   ],
                 )
