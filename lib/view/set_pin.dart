1049f142907fb5ce9eff1325fb84be88c806e34d
Rajeshwer Sahani <rajeshwersahani720@gmail.com>
2025-09-18 11:31:41 +0530

    set pin  changes

--- lib/view/set_pin.dart
+++ lib/view/set_pin.dart
@@ -1,7 +1,6 @@
 import 'package:alieus_captials_09_08/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screen_lock/flutter_screen_lock.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
 // import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 
@@ -44,30 +43,9 @@ class _set_pinState extends State<set_pin> {
                     Image.asset("assets/set_pin/ellipse_copy.png"), 
 
                     Positioned(top: 234,left: 105,
-                      child: Text("Enter your login passcode",style: TextStyle(color: Colors.white,fontSize: 17),
-                      ) 
-                      ),
+                      child: Text("Enter your login passcode",style: TextStyle(color: Colors.white,fontSize: 17),) )
 
-
-                    Positioned(
-                      child: PinCodeTextField(appContext: context, length: 4,
-                      obscureText: true,
-                       onChanged: (value) {
-                       enteredPin = value;
-                        },
-                         onCompleted: (value) {
-              if (value == correctPin) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Login Successful ✅")),
-                );
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Invalid PIN ❌")),
-                );
-              }
-            },
-                      )
-                      )
+                     
                       
                   ],
                 )
